f394745b9e0b3a546fcd000fe883bd2b77913ea7
JayashPatel <jayashpatel898@gmail.com>
2025-12-14 10:34:47 +0530

    UI Port Done

---
+++ Weather.js
@@ -0,0 +1,18 @@
+let searchBtn = document.querySelector("#search-btn")
+let result = document.querySelector("#result")
+
+
+const getWeatherData = async () => {
+    try{
+        let cityName =  document.querySelector("#city-name").value.trim();
+
+        if(!cityName) {
+            result.innerHTML = 
+            `<p class ="text-red-700 text-center mt-4 front-semibold">Please Enter a city Name..</p>`;
+        }
+    }catch(error) {
+        console.log(error,"Error in Fetching Weather Details");
+    }
+};
+
+searchBtn.addEventListener("click", getWeatherData);
